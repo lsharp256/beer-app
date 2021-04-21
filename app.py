9949a8e856422072743914228fc0d4e46dc49fb3
leroy <Lsharp256@gmail.com>
2021-04-21 23:14:37 +0200

    Added more info about the beer

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import json
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def get_beer():
 	r = requests.get('https://api.punkapi.com/v2/beers/random')
@@ -12,8 +13,10 @@ def get_beer():
 	beer = {
 		'name': beerson[0]['name'],
 		'abv': beerson[0]['abv'],
+		'first_brewed': beerson[0]['first_brewed'],
 		'description': beerson[0]['description'],
-		'food_pairing': beerson[0]['food_pairing'][0]
+		'food_pairing': beerson[0]['food_pairing'][0],
+		'yeast': beerson[0]['ingredients']['yeast']
 	}
 
 	return render_template('index.html', beer=beer)
